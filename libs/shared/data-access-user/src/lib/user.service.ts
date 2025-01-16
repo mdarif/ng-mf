8dd06c68c0e0352e2a11c0f29831f7f024ac3c23
Mohammad Arif <arif_iq@yahoo.co.in>
2025-01-16 09:51:50 +0530

    Add comment for the user service

--- libs/shared/data-access-user/src/lib/user.service.ts
+++ libs/shared/data-access-user/src/lib/user.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+/**
+ * This service is used to manage the user state.
+ */
 @Injectable({ providedIn: 'root' })
 export class UserService {
+  /**
+   * This is a private BehaviorSubject that is used to store the user's login state.
+   */
   private isUserLoggedIn = new BehaviorSubject(false);
-  isUserLoggedIn$ = this.isUserLoggedIn.asObservable();
+  /**
+   * This is a public observable that can be used to subscribe to the user's login state.
+   */
+  public isUserLoggedIn$ = this.isUserLoggedIn.asObservable();
 
+  /**
+   * Check the user's credentials and set the user's login state.
+   * 
+   * @param username 
+   * @param password 
+   */
   checkCredentials(username: string, password: string) {
     if (username === 'demo' && password === 'demo') {
       this.isUserLoggedIn.next(true);
@@ -14,6 +29,9 @@ export class UserService {
     }
   }
 
+  /**
+   * Log the user out by setting the user's login state to false.
+   */
   logout() {
     this.isUserLoggedIn.next(false);
   }
