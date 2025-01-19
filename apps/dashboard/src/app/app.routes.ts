cce18c878ed34f7c32d885d261a4166b2d1b13b4
Mohammad Arif <arif_iq@yahoo.co.in>
2025-01-19 13:18:35 +0530

    Revert to loadRemoteModule as loadRemote don't work with dynamic federation

--- apps/dashboard/src/app/app.routes.ts
+++ apps/dashboard/src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Route } from '@angular/router';
-import { loadRemoteModule } from '@nx/angular/mf';
+// import { loadRemote } from '@module-federation/enhanced/runtime';
 import { AppComponent } from './app.component';
+import { loadRemoteModule } from '@nx/angular/mf';
 
 export const appRoutes: Route[] = [
   {
     path: 'login',
+    // Dynamically load remote routes using the updated `loadRemote` API
     loadChildren: () =>
       loadRemoteModule('login', './Routes').then((m) => m.remoteRoutes),
+    // loadChildren: () =>
+    //   loadRemote<typeof import('login/Routes')>('login/Routes').then(
+    //     (m) => m?.remoteRoutes ?? []
+    //   ),
   },
   {
     path: '',
